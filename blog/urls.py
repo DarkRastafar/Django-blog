95592112db7ad1d92b8f2cede33bac9fd8a2f9dd
Your Name <you@example.com>
2021-07-23 19:35:21 +0600

    fix send_message

--- blog/urls.py
+++ blog/urls.py
@@ -6,9 +6,10 @@ app_name = 'blog' #определение пространства имен
 
 urlpatterns = [
 	
-	path('', views.post_list, name='post_list'), # не принимает аргументов - только сопоставляется с обработчиком post_list
+	path('', views.PostListView.as_view(), name='post_list'), # не принимает аргументов - только сопоставляется с обработчиком post_list
 	path('<int:year>/<int:month>/<int:day>/<slug:post>/', 
 		  views.post_detail, 
 		  name='post_detail'),
+	path('<int:post_id>/share/', views.post_share, name='post_share'),
 	#вызывает post_detail и принимает параметры. "<>" - используется для извлечения значений из URLа.
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -124,3 +124,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'alwaysintouch247@gmail.com'
+EMAIL_HOST_PASSWORD = '42o2o9143988z'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
