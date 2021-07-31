b2ea9922b31250434e25f5e32c921f9b2c150b7f
Your Name <you@example.com>
2021-07-31 18:35:38 +0600

    добавил возможность подписки на RSS рассылку

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .feeds import LatestPostsFeed
 from . import views
 
 
@@ -10,6 +11,6 @@ urlpatterns = [
 	path('<int:year>/<int:month>/<int:day>/<slug:post>/', 
 		  views.post_detail, name='post_detail'),
 	path('<int:post_id>/share/', views.post_share, name='post_share'),
-	path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
-	#вызывает post_detail и принимает параметры. "<>" - используется для извлечения значений из URLа.
+	path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),#вызывает post_detail и принимает параметры. "<>" - используется для извлечения значений из URLа.
+	path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
