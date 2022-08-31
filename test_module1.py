65aee61bbe14b028679ba523060b6db9bf2bc75d
DarkRastafar <mvmsurgutmebel@gmail.com>
2022-08-31 11:06:48 +0500

    "тест на русские буквы"

--- test_module1.py
+++ test_module1.py
@@ -5,3 +5,7 @@ class SoloFunctionsTestCase(TestCase):
     def test_test(self):
         print('Assert complete!')
         self.assertEqual(1, 1)
+
+    def test_russian_chr(self):
+        print('Тест')
+        self.assertEqual('Тест', 'Тест')
